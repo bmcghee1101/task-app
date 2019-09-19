32a9e004878c83ce3e8a9f8e3703ba1164cf8378
Brendan McGhee <b.mcghee.1101@gmail.com>
2019-09-18 23:52:42 -0500

    use mongoose

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/users')
+const Task = require('./models/tasks')
+
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+
+// Create User
+app.post('/users', (req, res) => {
+	const user = new User(req.body)
+	user.save().then(() => {
+		res.status(201).send(user)
+	}).catch((e) => {
+		res.status(400).send(e)
+	})
+})
+
+// Create Task
+app.post('/tasks', (req, res) => {
+	const task = new Task(req.body)
+	task.save().then(() => {
+		res.status(201).send(task)
+	}).catch((e) => {
+		res.status(400).send(e)
+	})
+})
+
+
+app.listen(port, () => {
+	console.log('Server is up on port ' + port)
+})
+
