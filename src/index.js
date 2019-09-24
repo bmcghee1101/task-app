cee9f5c42e3a6de70263bd5069df265eaeaaa067
Brendan McGhee <b.mcghee.1101@gmail.com>
2019-09-24 00:09:54 -0500

    Finished route handlers

--- src/index.js
+++ src/index.js
@@ -1,86 +1,23 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/users')
+const userRouter = require('./routers/user')
 const Task = require('./models/tasks')
+const taskRouter = require('./routers/task')
 
 
 const app = express()
 const port = process.env.PORT || 3000
 
+
+
+// app.use((req, res) => {
+// 	res.status(503).send('Site currently under maintenance!')
+// })
+
 app.use(express.json())
-
-
-// Create User
-app.post('/users', (req, res) => {
-	const user = new User(req.body)
-	user.save().then(() => {
-		res.status(201).send(user)
-	}).catch((e) => {
-		res.status(400).send(e)
-	})
-})
-
-// Get all users
-app.get('/users', (req, res) => {
-	User.find({}).then((users) => {
-		res.send(users)
-	}).catch((e) => {
-		res.status(500).send()
-	})
-})
-
-// Get user by id
-app.get('/users/:id', (req, res) => {
-	const _id = req.params.id
-	
-	User.findById(_id).then((user) => {
-		if (!user) {
-			return res.status(404).send()
-		}
-		res.send(user)
-	}).catch((e) => {
-		res.status(500).send()
-	})
-})
-
-
-
-// Create Task
-app.post('/tasks', (req, res) => {
-	const task = new Task(req.body)
-	task.save().then(() => {
-		res.status(201).send(task)
-	}).catch((e) => {
-		res.status(400).send(e)
-	})
-})
-
-// Get all tasks
-app.get('/tasks', (req, res) => {
-	Task.find({}).then((tasks) => {
-		res.send(tasks)
-	}).catch((e) => {
-		res.status(500).send()
-	})
-})
-
-// Get task by id
-app.get('/tasks/:id', (req, res) => {
-	const _id = req.params.id
-	
-	Task.findById(_id).then((task) => {
-		if (!task) {
-			return res.status(404).send()
-		}
-		res.send(task)
-	}).catch((e) => {
-		res.status(500).send()
-	})
-})
-
-
-
-
+app.use(userRouter)
+app.use(taskRouter)
 
 
 
@@ -88,4 +25,5 @@ app.get('/tasks/:id', (req, res) => {
 app.listen(port, () => {
 	console.log('Server is up on port ' + port)
 })
+
 
--- src/models/users.js
+++ src/models/users.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./tasks')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
@@ -9,6 +12,7 @@ const User = mongoose.model('User', {
 	},
 	email: {
 		type: String,
+		unique: true,
 		required: true,
 		trim: true,
 		lowercase: true,
@@ -37,7 +41,82 @@ const User = mongoose.model('User', {
 				throw new Error('Password cannot be password')
 			}
 		}
-	}
+	},
+	tokens: [{
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 })
+
+
+userSchema.virtual('tasks', {
+	ref: 'Task',
+	localField: '_id',
+	foreignField: 'owner'
+})
+
+
+
+userSchema.methods.generateAuthToken = async function () {
+	const user = this
+	const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+	user.tokens = user.tokens.concat({ token })
+	await user.save()
+
+	return token
+}
+
+userSchema.methods.toJSON = function () {
+	const user = this
+	const userObject = user.toObject()
+
+	delete userObject.password
+	delete userObject.tokens
+
+	return userObject
+}
+
+
+userSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email })
+
+	if (!user) {
+		throw new Error('Unable to login')
+	}
+
+	const isMatch = await bcrypt.compare(password, user.password)
+
+	if (!isMatch) {
+		throw new Error('Unable to login')
+	}
+
+	return user
+}
+
+
+
+// hash plain text
+userSchema.pre('save', async function (next) {
+	const user = this
+
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8)
+	}
+
+	next()
+})
+
+//Delete user task when user delete
+userSchema.pre('remove', async function (next) {
+	const user = this
+	await Task.deleteMany({ owner: user._id })
+	next()
+})
+
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
